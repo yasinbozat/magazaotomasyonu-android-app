f09f6dd7a84698a8b89e7ea50e3c12c9a85f87ba
Yasin Bozat <yasinbozatr@gmail.com>
2019-07-23 22:19:12 +0300

    Uploaded Files

--- MagazaOtomasyon/app/src/main/java/com/yofyx/magazaotomasyon/MainActivity.java
+++ MagazaOtomasyon/app/src/main/java/com/yofyx/magazaotomasyon/MainActivity.java
@@ -3,12 +3,18 @@ package com.yofyx.magazaotomasyon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_AddProduct,btn__DeleteProduct,btn_UpdateProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
