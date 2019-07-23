83438707d04cf8283aca09466ba3694932e9f142
Yasin Bozat <yasinbozatr@gmail.com>
2019-07-23 22:23:27 +0300

    Upload Changes

--- MagazaOtomasyon/app/src/main/java/com/yofyx/magazaotomasyon/MainActivity.java
+++ MagazaOtomasyon/app/src/main/java/com/yofyx/magazaotomasyon/MainActivity.java
@@ -3,18 +3,45 @@ package com.yofyx.magazaotomasyon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn_AddProduct,btn__DeleteProduct,btn_UpdateProduct;
+    Button btn_AddProduct,btn_DeleteProduct,btn_UpdateProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //FULL SCREEN CODES ------------------------------------------------------------------------
         getSupportActionBar().hide();
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //------------------------------------------------------------------------------------------
+
+        //BUTTONS ----------------------------------------------------------------------------------
+        btn_AddProduct = findViewById(R.id.btn_AddProduct);
+        btn_DeleteProduct = findViewById(R.id.btn_DeleteProduct);
+        //------------------------------------------------------------------------------------------
+
+        btn_AddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        
+
+        btn_DeleteProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+
+
     }
 }
