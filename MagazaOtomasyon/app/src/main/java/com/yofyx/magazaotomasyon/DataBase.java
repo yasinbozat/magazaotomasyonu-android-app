bfb504ea8bc6e301321ccce392748f19bab94689
fatihsuzen <53154457+fatihsuzen@users.noreply.github.com>
2019-07-28 16:02:27 +0300

    Delete activity updated
    delete activity layout designed.

--- MagazaOtomasyon/app/src/main/java/com/yofyx/magazaotomasyon/DataBase.java
+++ MagazaOtomasyon/app/src/main/java/com/yofyx/magazaotomasyon/DataBase.java
@@ -62,6 +62,15 @@ public class DataBase extends SQLiteOpenHelper {
         }
         db.close();
     }
-
+    public void VeriSil(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        try {
+            // id ye göre verimizi siliyoruz
+            String where = ROW_ID + " = " + id ;
+            db.delete(TABLE_URUNLER,where,null);
+        }catch (Exception e){
+        }
+        db.close();
+    }
 
 }
